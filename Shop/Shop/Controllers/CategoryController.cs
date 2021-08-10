944dacc7d6273a35289f257810d4d23a4cdc21dd
Rafael Hiroshi <hiroshi_nuts@hotmail.com>
2021-08-10 15:17:37 -0300

    Entitys + Basic Controller

---
+++ Shop/Shop/Controllers/CategoryController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Shop.Models;
+
+namespace Shop.Controllers
+{
+    [Route("categories")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        [HttpGet]
+        [Route("")]
+        public async Task<ActionResult<List<Category>>> Get()
+        {
+            return new List<Category>();
+        }
+
+        [HttpGet]
+        [Route("{id:int}")]
+        public async Task<ActionResult<Category>> GetById(int id)
+        {
+            return new Category();
+        }
+
+        [HttpPost]
+        [Route("")]
+        public async Task<ActionResult<Category>> Post([FromBody] Category model)
+        {
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            return Ok(model);
+        }
+
+        [HttpPut]
+        [Route("{id:int}")]
+        public async Task<ActionResult<Category>> Put(int id, [FromBody] Category model)
+        {
+            //Verifica se o ID informado é o mesmo do modelo
+            if(model.Id != id) 
+                return NotFound(new { message = "Categoria não encontrada" });
+
+            //Verifica se os dados são validos
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            return Ok(model);
+        }
+
+        [HttpDelete]
+        [Route("{id:int}")]
+        public async Task<ActionResult<Category>> Delete()
+        {
+            return Ok();
+        }
+
+
+
+    }
+}
---
+++ Shop/Shop/Models/Category.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="Este campo é obrigatório")]
+        [MaxLength(60, ErrorMessage = "Este campo deve conter entre 3 e 60 caracteres.")]
+        [MinLength(3, ErrorMessage = "Este campo deve conter entre 3 e 60 caracteres.")]        
+        public string Title { get; set; }
+    }
+}
