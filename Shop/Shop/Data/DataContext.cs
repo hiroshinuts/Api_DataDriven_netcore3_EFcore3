e67a45b740dfb15a56b02b2f226307be43a19669
Rafael Hiroshi <hiroshi_nuts@hotmail.com>
2021-08-10 15:51:57 -0300

    DataContext and Dependency Injection

--- Shop/Shop/Data/DataContext.cs
+++ Shop/Shop/Data/DataContext.cs
@@ -1,12 +1,25 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using Shop.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Shop.Data
 {
-    public class DataContext
+    public class DataContext : DbContext
     {
+
+        public DataContext(DbContextOptions<DataContext> options): base(options)
+        {
+            
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<Category> Categories { get; set; }
+
+        public DbSet<User> Users { get; set; }
 
     }
 }
