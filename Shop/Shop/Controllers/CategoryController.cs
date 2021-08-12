eb9b994a9b15655101c162313076d0f763b6409e
Rafael Hiroshi <hiroshi_nuts@hotmail.com>
2021-08-11 22:19:52 -0300

    Crud Basico - InMemory

--- Shop/Shop/Controllers/CategoryController.cs
+++ Shop/Shop/Controllers/CategoryController.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Shop.Data;
 using Shop.Models;
 
 namespace Shop.Controllers
@@ -14,36 +17,63 @@ namespace Shop.Controllers
     {
         [HttpGet]
         [Route("")]
-        public async Task<ActionResult<List<Category>>> Get()
+        public async Task<ActionResult<List<Category>>> Get([FromServices] DataContext context)
         {
-            return new List<Category>();
+            var categories = await context.Categories.AsNoTracking().ToListAsync();
+            return Ok(categories);
         }
 
         [HttpGet]
         [Route("{id:int}")]
-        public async Task<ActionResult<Category>> GetById(int id)
+        public async Task<ActionResult<Category>> GetById(int id, [FromServices] DataContext context)
         {
-            return new Category();
+            var categories = await context.Categories.AsNoTracking().FirstOrDefaultAsync(x => x.Id == id);
+            return Ok(categories);
         }
 
         [HttpPost]
         [Route("")]
-        public async Task<ActionResult<Category>> Post([FromBody] Category model)
+        public async Task<ActionResult<Category>> Post([FromBody] Category model, [FromServices] DataContext context)
         {
 
             if (!ModelState.IsValid)
                 return BadRequest(ModelState);
 
-            return Ok(model);
+            try
+            {
+                context.Categories.Add(model);
+                await context.SaveChangesAsync();
+                return Ok(model);
+            }
+            catch
+            {
+                return BadRequest(new { message = "Não foi possivel criar a categoria" });
+            }
+          
         }
 
         [HttpPut]
         [Route("{id:int}")]
-        public async Task<ActionResult<Category>> Put(int id, [FromBody] Category model)
+        public async Task<ActionResult<Category>> Put(int id, [FromBody] Category model, [FromServices] DataContext context)
         {
             //Verifica se o ID informado é o mesmo do modelo
             if(model.Id != id) 
                 return NotFound(new { message = "Categoria não encontrada" });
+
+            try
+            {
+                context.Entry<Category>(model).State = EntityState.Modified;
+                await context.SaveChangesAsync();
+                return Ok(model);
+            }
+            catch(DbUpdateConcurrencyException)
+            {
+                return BadRequest( new { message = "Este registro já foi atualizado" });
+            }
+            catch (Exception)
+            {
+                return BadRequest(new { message = "Não foi possivel atualizar a categoria" });
+            }
 
             //Verifica se os dados são validos
             if (!ModelState.IsValid)
@@ -54,9 +84,24 @@ namespace Shop.Controllers
 
         [HttpDelete]
         [Route("{id:int}")]
-        public async Task<ActionResult<Category>> Delete()
+        public async Task<ActionResult<Category>> Delete(int id, [FromServices]DataContext context)
         {
-            return Ok();
+            try
+            {
+                var category = await context.Categories.FirstOrDefaultAsync(x => x.Id == id);
+                if (category == null)
+                    return NotFound(new { message = "Categoria não encontrada" });
+
+                context.Categories.Remove(category);
+                await context.SaveChangesAsync();
+                
+                return Ok(category);
+            }
+            catch (Exception)
+            {
+                return BadRequest(new { message = "Não foi possivel remover a categoria" });
+            }
+    
         }
 
 
