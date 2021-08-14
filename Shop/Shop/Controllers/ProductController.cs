09fea3dac4f140f4af7785ed44db778fb3b90806
Rafael Hiroshi <hiroshi_nuts@hotmail.com>
2021-08-14 20:50:15 -0300

    Versionamento  + Cache + ZipResponse

--- Shop/Shop/Controllers/ProductController.cs
+++ Shop/Shop/Controllers/ProductController.cs
@@ -11,7 +11,7 @@ using Shop.Models;
 
 namespace Shop.Controllers
 {
-    [Route("products")]
+    [Route("v1/products")]
     [ApiController]
     public class ProductController : ControllerBase
     {
--- Shop/Shop/Controllers/UserController.cs
+++ Shop/Shop/Controllers/UserController.cs
@@ -12,7 +12,7 @@ using Shop.Services;
 
 namespace Shop.Controllers
 {
-    [Route("users")]
+    [Route("v1/users")]
     public class UserController : Controller
     {
         [HttpGet]
